da4052444e6bf3011f5d30e2332304ae0cd5e11e
Ben Walters <walters.benj@gmail.com>
2016-12-17 20:37:14 -0600

    Removed .yml seeds to utilize .json

--- app/controllers/v1/episodes_controller.rb
+++ app/controllers/v1/episodes_controller.rb
@@ -4,12 +4,7 @@ module V1
             @episodes = Episode.all
 
             if params[:search]
-                searchSplit = params[:search].gsub(/\s+/m, '|')
-                # SELECT "episodes".* FROM "episodes" WHERE (episode_json @> '{"series_number": "1"}')
-                # SELECT "episodes".* FROM "episodes" WHERE (episode_json ? 'title')
-                # SELECT * FROM episodes WHERE (episode_json -> 'title' ? 'Series 1, Episode 1')
-                # SELECT * FROM episodes WHERE episode_json::text LIKE '%Pagani%'
-                @episodes = @episodes.where(["episode_json::text SIMILAR TO ?", "%(#{searchSplit})%"])
+                @episodes = @episodes.where(["LOWER(episode_json::text) LIKE LOWER(?)", "%#{params[:search]}%"])
             end
         end
 
