364586ed8e78d0e4557e7b57217e8b177edd7eb6
Ben Walters <Ben Walters>
2016-12-28 22:52:41 -0600

    Added support for verbatim searches, refactored search parameter logic to allow for more generic searching

--- app/controllers/v1/episodes_controller.rb
+++ app/controllers/v1/episodes_controller.rb
@@ -1,28 +1,112 @@
 module V1
     class EpisodesController < ApplicationController
         def index
-            searchTerm = ""
+            rawSearchTerms = ""
+            searchTermsArr = []
             searchTime = DateTime.now
+            loggingEnabled = true
 
             @episodes = Episode.all
 
             if params[:search]
-                searchTerm = params[:search]
-                searchTerms = params[:search].split(",")
+                rawSearchTerms = params[:search]
+                searchTerms = rawSearchTerms
 
-                searchTerms.each do |searchTerm|
-                    @episodes = @episodes.where(["LOWER(episode_json::text) LIKE LOWER(?)", "%#{searchTerm}%"])
+                # Match strings in quotes as verbatim
+                verbatimSearchTermsArr = searchTerms.scan(/"([^"]*)"/).flatten
+                verbatimSearchTermsArr.each do |verbatimSearchTerm|
+                    @episodes = @episodes.where("episode_json::text ~* ?", "\\m#{verbatimSearchTerm}\\M")
+
+                    # Remove to prevent duplicate parsing
+                    searchTerms = searchTerms.gsub(verbatimSearchTerm, "")
+                end
+
+                # Cleanse input
+                searchTerms = searchTerms.gsub("\"", "")
+                searchTerms = searchTerms.strip
+
+                # Build an array of terms split by whitespace (match as AND)
+                searchTermsArr = searchTerms.split(" ")
+
+                searchTermsArr.each do |searchTerm|
+                    # Build an array of terms split by commas (match AS OR)
+                    subSearchTermsArr = searchTerm.split(",")
+
+                    if subSearchTermsArr.count > 1
+                        queryString = ""
+
+                        subSearchTermsArr.each_with_index do |subSearchTerm, index|
+                            if index == 0
+                                queryString << "LOWER(episode_json::text) LIKE LOWER(?)"
+                            else
+                                queryString << " OR LOWER(episode_json::text) LIKE LOWER(?)"
+                            end
+                        end
+
+                        subSearchTermsArr2 = subSearchTermsArr.map {|subSearchTerm| "%" + subSearchTerm + "%"}
+
+                        @episodes = @episodes.where(queryString, *subSearchTermsArr2)
+                    else
+                        @episodes = @episodes.where("LOWER(episode_json::text) LIKE LOWER(?)", "%#{searchTerm}%")
+                    end
                 end
             end
 
-            log = Log.new
-            log.search_term = searchTerm
-            log.search_time = searchTime
-            log.save
+            if params[:logging]
+                loggingEnabled = params[:logging] == "1"
+            end
+
+            if loggingEnabled
+                log = Log.new
+                log.search_term = rawSearchTerms
+                log.search_time = searchTime
+                log.save
+            end
         end
 
         def show
             @episode = Episode.find(params[:id])
         end
+
+        private
+            def getAllDictionaries
+                return Dictionary.all
+            end
+
+            def getAllRelatedTerms
+                allRelatedTerms = []
+                allDictionaries = getAllDictionaries
+
+                allDictionaries.each do |dictionary|
+                    relatedTerms = dictionary.related_terms.split(",")
+                    allRelatedTerms.concat relatedTerms
+                end
+
+                return allRelatedTerms
+            end
+
+            def containsRelatedTerm(input)
+                allRelatedTerms = getAllRelatedTerms
+
+                allRelatedTerms.each do |relatedTerm|
+                    if input.include? relatedTerm
+                        return true
+                    end
+                end
+
+                return false
+            end
+
+            def extractRelatedTerm(input)
+                allRelatedTerms = getAllRelatedTerms
+
+                allRelatedTerms.each do |relatedTerm|
+                    if input.include? relatedTerm
+                        return relatedTerm
+                    end
+                end
+
+                return ""
+            end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,93 +1,93 @@
 module V1
-    series_1_seed = Rails.root.join('db','seeds', 'series_1.json')
+    series_1_seed = Rails.root.join('db','series', 'series_1.json')
     series_1_json = JSON.parse(File.read(series_1_seed))
     Episode.create!(series_1_json['episodes'])
 
-    series_2_seed = Rails.root.join('db', 'seeds', 'series_2.json')
+    series_2_seed = Rails.root.join('db', 'series', 'series_2.json')
     series_2_json = JSON.parse(File.read(series_2_seed))
     Episode.create!(series_2_json['episodes'])
 
-    series_3_seed = Rails.root.join('db','seeds', 'series_3.json')
+    series_3_seed = Rails.root.join('db','series', 'series_3.json')
     series_3_json = JSON.parse(File.read(series_3_seed))
     Episode.create!(series_3_json['episodes'])
 
-    series_4_seed = Rails.root.join('db','seeds', 'series_4.json')
+    series_4_seed = Rails.root.join('db','series', 'series_4.json')
     series_4_json = JSON.parse(File.read(series_4_seed))
     Episode.create!(series_4_json['episodes'])
 
-    series_5_seed = Rails.root.join('db','seeds', 'series_5.json')
+    series_5_seed = Rails.root.join('db','series', 'series_5.json')
     series_5_json = JSON.parse(File.read(series_5_seed))
     Episode.create!(series_5_json['episodes'])
 
-    series_6_seed = Rails.root.join('db','seeds', 'series_6.json')
+    series_6_seed = Rails.root.join('db','series', 'series_6.json')
     series_6_json = JSON.parse(File.read(series_6_seed))
     Episode.create!(series_6_json['episodes'])
 
-    series_7_seed = Rails.root.join('db','seeds', 'series_7.json')
+    series_7_seed = Rails.root.join('db','series', 'series_7.json')
     series_7_json = JSON.parse(File.read(series_7_seed))
     Episode.create!(series_7_json['episodes'])
 
-    series_8_seed = Rails.root.join('db','seeds', 'series_8.json')
+    series_8_seed = Rails.root.join('db','series', 'series_8.json')
     series_8_json = JSON.parse(File.read(series_8_seed))
     Episode.create!(series_8_json['episodes'])
 
-    series_9_seed = Rails.root.join('db','seeds', 'series_9.json')
+    series_9_seed = Rails.root.join('db','series', 'series_9.json')
     series_9_json = JSON.parse(File.read(series_9_seed))
     Episode.create!(series_9_json['episodes'])
 
-    series_10_seed = Rails.root.join('db','seeds', 'series_10.json')
+    series_10_seed = Rails.root.join('db','series', 'series_10.json')
     series_10_json = JSON.parse(File.read(series_10_seed))
     Episode.create!(series_10_json['episodes'])
 
-    series_11_seed = Rails.root.join('db','seeds', 'series_11.json')
+    series_11_seed = Rails.root.join('db','series', 'series_11.json')
     series_11_json = JSON.parse(File.read(series_11_seed))
     Episode.create!(series_11_json['episodes'])
 
-    series_12_seed = Rails.root.join('db','seeds', 'series_12.json')
+    series_12_seed = Rails.root.join('db','series', 'series_12.json')
     series_12_json = JSON.parse(File.read(series_12_seed))
     Episode.create!(series_12_json['episodes'])
 
-    series_13_seed = Rails.root.join('db','seeds', 'series_13.json')
+    series_13_seed = Rails.root.join('db','series', 'series_13.json')
     series_13_json = JSON.parse(File.read(series_13_seed))
     Episode.create!(series_13_json['episodes'])
 
-    series_14_seed = Rails.root.join('db','seeds', 'series_14.json')
+    series_14_seed = Rails.root.join('db','series', 'series_14.json')
     series_14_json = JSON.parse(File.read(series_14_seed))
     Episode.create!(series_14_json['episodes'])
 
-    series_15_seed = Rails.root.join('db','seeds', 'series_15.json')
+    series_15_seed = Rails.root.join('db','series', 'series_15.json')
     series_15_json = JSON.parse(File.read(series_15_seed))
     Episode.create!(series_15_json['episodes'])
 
-    series_16_seed = Rails.root.join('db','seeds', 'series_16.json')
+    series_16_seed = Rails.root.join('db','series', 'series_16.json')
     series_16_json = JSON.parse(File.read(series_16_seed))
     Episode.create!(series_16_json['episodes'])
 
-    series_17_seed = Rails.root.join('db','seeds', 'series_17.json')
+    series_17_seed = Rails.root.join('db','series', 'series_17.json')
     series_17_json = JSON.parse(File.read(series_17_seed))
     Episode.create!(series_17_json['episodes'])
 
-    series_18_seed = Rails.root.join('db','seeds', 'series_18.json')
+    series_18_seed = Rails.root.join('db','series', 'series_18.json')
     series_18_json = JSON.parse(File.read(series_18_seed))
     Episode.create!(series_18_json['episodes'])
 
-    series_19_seed = Rails.root.join('db','seeds', 'series_19.json')
+    series_19_seed = Rails.root.join('db','series', 'series_19.json')
     series_19_json = JSON.parse(File.read(series_19_seed))
     Episode.create!(series_19_json['episodes'])
 
-    series_20_seed = Rails.root.join('db','seeds', 'series_20.json')
+    series_20_seed = Rails.root.join('db','series', 'series_20.json')
     series_20_json = JSON.parse(File.read(series_20_seed))
     Episode.create!(series_20_json['episodes'])
 
-    series_21_seed = Rails.root.join('db','seeds', 'series_21.json')
+    series_21_seed = Rails.root.join('db','series', 'series_21.json')
     series_21_json = JSON.parse(File.read(series_21_seed))
     Episode.create!(series_21_json['episodes'])
 
-    series_22_seed = Rails.root.join('db','seeds', 'series_22.json')
+    series_22_seed = Rails.root.join('db','series', 'series_22.json')
     series_22_json = JSON.parse(File.read(series_22_seed))
     Episode.create!(series_22_json['episodes'])
 
-    series_23_seed = Rails.root.join('db','seeds', 'series_23.json')
+    series_23_seed = Rails.root.join('db','series', 'series_23.json')
     series_23_json = JSON.parse(File.read(series_23_seed))
     Episode.create!(series_23_json['episodes'])
 end
